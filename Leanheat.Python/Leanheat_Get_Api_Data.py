faa49387543527e369842b07489b44828136ba58
User0183275 <PapaCode@users.noreply.github.com>
2021-04-16 12:55:59 +0200

    Rettet i python koden
    Jeg har rettet noget i python og gjort det lidt pænere.

--- Leanheat.Python/Leanheat_Get_Api_Data.py
+++ Leanheat.Python/Leanheat_Get_Api_Data.py
@@ -1,4 +1,3 @@
-
 import requests
 from requests.auth import HTTPBasicAuth
 import json
@@ -11,14 +10,12 @@ if response.status_code == 401:
     response = requests.get('https://api.leanheat.fi/v2/sites/aggregateData?site_name=lh_lysholtalle11a_vejle&data=apt_temperature&start_time=1559347200', auth=HTTPBasicAuth('dandy-leanheat-api-key', 'GZXXOHQ90LJSJDBKUC19R0QYFWOAUX67KZZGLK5UOPPA0EG3MCNT9TQEOIEEDPJ1TR88BBWHEMLCCNRL7CCCZFRUOSZN9HIAG078Y3254NP44XL6NELAKYN7UMXO47DI'))
     with open("leanheatapidata.json", "w") as f:
         f.write(response.text)
+        print("json file saved!")
 
 #Open the json file
 with open("leanheatapidata.json") as f:
     data = json.load(f)
 
-#Makes the json file more readable
-with open("leanheatapidata.json", "w") as f:
-    json.dump(data, f, indent=2)
 
 #Remove all the unnecessary stuff
 for temp in data:
@@ -28,6 +25,12 @@ for temp in data:
     del temp['location_alias']
     del temp['data_version']
 
-with open('leanheatapidata.json', 'w') as f:
+
+#Makes the json file more readable
+with open("leanheatapidata.json", "w") as f:
     json.dump(data, f, indent=2)
-    print("json file saved!")
+
+#Saves it to a CSV file
+with open('leanheatapidata.csv', 'w') as f:
+    json.dump(data, f, indent=2)
+    print("csv file saved!")
