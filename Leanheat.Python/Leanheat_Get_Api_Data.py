cccfe98b2f3468b7ecc60b014c532880aee88dfd
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-04-16 10:52:09 +0200

    Add project files.

---
+++ Leanheat.Python/Leanheat_Get_Api_Data.py
@@ -0,0 +1,33 @@
+
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+#API
+response = requests.get('https://api.leanheat.fi/v2/sites/metadata')
+
+#Access the data in the api and puts its in a json file
+if response.status_code == 401:    
+    response = requests.get('https://api.leanheat.fi/v2/sites/aggregateData?site_name=lh_lysholtalle11a_vejle&data=apt_temperature&start_time=1559347200', auth=HTTPBasicAuth('dandy-leanheat-api-key', 'GZXXOHQ90LJSJDBKUC19R0QYFWOAUX67KZZGLK5UOPPA0EG3MCNT9TQEOIEEDPJ1TR88BBWHEMLCCNRL7CCCZFRUOSZN9HIAG078Y3254NP44XL6NELAKYN7UMXO47DI'))
+    with open("leanheatapidata.json", "w") as f:
+        f.write(response.text)
+
+#Open the json file
+with open("leanheatapidata.json") as f:
+    data = json.load(f)
+
+#Makes the json file more readable
+with open("leanheatapidata.json", "w") as f:
+    json.dump(data, f, indent=2)
+
+#Remove all the unnecessary stuff
+for temp in data:
+    del temp['series_id']
+    del temp['series_loc']
+    del temp['location_id']
+    del temp['location_alias']
+    del temp['data_version']
+
+with open('leanheatapidata.json', 'w') as f:
+    json.dump(data, f, indent=2)
+    print("json file saved!")
