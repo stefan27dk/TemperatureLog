a8c15bf3a01ed8cb4b881fdb26e5222fc77e5e7b
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-04-20 14:24:05 +0200

    Updated Administrator Controller ---> GetUser, Get All User with roles, GetAll Users, remove Role should be redon with transaction or Unit of Work

--- Leanheat.Identity.API/Controllers/AdministratorController.cs
+++ Leanheat.Identity.API/Controllers/AdministratorController.cs
@@ -1,7 +1,9 @@
-﻿using Leanheat.Identity.API.Models;
+﻿using Leanheat.Identity.API.DBContexts;
+using Leanheat.Identity.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,16 +21,19 @@ namespace Leanheat.Identity.API.Controllers
         // Managers
         private readonly RoleManager<IdentityRole> roleManager; // RoleManager
         private readonly UserManager<ApplicationUser> userManager; // User Manager
+        private readonly LeanheatIdentityApiContext identityDBContext; // Db Context
 
 
 
 
 
         // || Constructor || ====================================================================
-        public AdministratorController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        public AdministratorController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, 
+            LeanheatIdentityApiContext identityDBContext)
         {
             this.roleManager = roleManager;
             this.userManager = userManager;
+            this.identityDBContext = identityDBContext;
         }
 
 
@@ -52,7 +57,7 @@ namespace Leanheat.Identity.API.Controllers
                 {
                     return StatusCode(200, "Role Created Successfully");
                 }
-                return StatusCode(409, result.Errors);
+                return StatusCode(500, result.Errors);
             }
             return StatusCode(409, "Input was null - Cant create Role without rolename");
         }
@@ -106,13 +111,15 @@ namespace Leanheat.Identity.API.Controllers
                 {
                     return StatusCode(200, "Role Updated Successfully");
                 }
-                return StatusCode(401, result.Errors);  // If Update Error
+                return StatusCode(500, result.Errors);  // If Update Error
             }
             else // If no such Role
             {
-                return StatusCode(401, "No such role");
+                return StatusCode(404, "No such role");
             }
         }
+
+
 
 
 
@@ -159,14 +166,14 @@ namespace Leanheat.Identity.API.Controllers
                                         await userManager.AddToRoleAsync(rollbackList[k], roleName);  // Rollback
                                       }
                                    }
-                                   return StatusCode(401, "Fatal error: could not reassign one or more of the users - Please try again"); // If list empty return just message
+                                   return StatusCode(500, "Fatal error: could not reassign one or more of the users - Please try again"); // If list empty return just message
                                }
 
                             }
                         }
                         else
                         {
-                           return StatusCode(401, $"The Role: \"{reAssignRole}\" for reassigning the users cant be found");
+                           return StatusCode(404, $"The Role: \"{reAssignRole}\" for reassigning the users cant be found");
                         }
                     }
                      
@@ -179,12 +186,17 @@ namespace Leanheat.Identity.API.Controllers
                         {
                            return StatusCode(200, $"Role: \"{roleName} \" was Successfully Deleted");  // Role Deleted msg
                         }
-                           return StatusCode(401, result.Errors); // Delete Errors
+                           return StatusCode(500, result.Errors); // Delete Errors
                     
             }
             return StatusCode(401,"Role not found"); // Role not found msg
 
          }
+
+
+
+
+
 
 
 
@@ -215,14 +227,82 @@ namespace Leanheat.Identity.API.Controllers
                     }
                     return StatusCode(200, "The User is already member of that role"); // Already memeber of that role msg
                 }
-                return StatusCode(401, "Role not found");  // Role not found
+                return StatusCode(404, "Role not found");  // Role not found
             }
-            return StatusCode(401, "User not found"); // User not found
+            return StatusCode(404, "User not found"); // User not found
         }
 
 
 
 
+
+
+
+
+        // ===== Get ALL Users - || GET || =====================================================================
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+              var users = await userManager.Users.ToListAsync();
+              return  StatusCode(200, users); // Get all users
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        // ===== Get User - || GET || =====================================================================
+        [HttpGet]
+        [Route("GetUser")]
+        public async Task<IActionResult> GetUser(string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+            if(user != null)
+            {
+                return StatusCode(200, user); // Get user
+            }
+            return StatusCode(404, "No such user"); // Get user
+
+        }
+
+
+
+
+
+
+
+
+        // ===== Get Users with Roles - || GET || =====================================================================
+        [HttpPost]
+        [Route("GetAllUsersWithRoles")]
+        public async Task<IActionResult> GetAllUsersWithRoles()
+        {
+            try
+            {
+                      return await Task.Run(() => 
+                      { 
+                         var users =  identityDBContext.UserRoles;
+                         return StatusCode(200, users); // Get all users
+                      });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
 
     }
 }
