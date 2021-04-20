60b885c2bd077d6c9c5cd32b33b73ac5b6f30dbe
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-04-20 12:57:22 +0200

    AdministratorController update --> > Remove Role with  kind of roll back

--- Leanheat.Identity.API/Controllers/AdministratorController.cs
+++ Leanheat.Identity.API/Controllers/AdministratorController.cs
@@ -38,7 +38,7 @@ namespace Leanheat.Identity.API.Controllers
 
 
 
-        // ===== Create Role =====================================================================
+        // ===== Create Role || POST || =====================================================================
         [HttpPost]
         [Route("CreateRole")]
         public async Task<IActionResult> CreateRole(string roleName)
@@ -67,14 +67,159 @@ namespace Leanheat.Identity.API.Controllers
 
 
 
-        // ===== Get All Roles =====================================================================
+
+
+        // ===== Get All Roles  || GET || =====================================================================
         [HttpGet]
         [Route("GetAllRoles")]
-        public IActionResult GetAllRoles()
+        public async Task<IActionResult> GetAllRoles()
         {
-            var roles = roleManager.Roles;
-            return new JsonResult(roles);
+           return await Task.Run(() =>
+           {
+               var roles = roleManager.Roles;
+               return new JsonResult(roles);
+           });
         }
+
+
+
+
+
+
+
+
+
+
+        // ===== Edit Role - || Post || =====================================================================
+        [HttpPost]
+        [Route("EditRole")]
+        public async Task<IActionResult> EditRole(string oldName, string newName)
+        {
+            // Find Role
+            var role = await roleManager.FindByNameAsync(oldName);
+            if(role != null) // If found
+            {     
+                role.Name = newName; // Change name
+                var result = await roleManager.UpdateAsync(role); // Update
+
+                if (result.Succeeded) // If Update OK
+                {
+                    return StatusCode(200, "Role Updated Successfully");
+                }
+                return StatusCode(401, result.Errors);  // If Update Error
+            }
+            else // If no such Role
+            {
+                return StatusCode(401, "No such role");
+            }
+        }
+
+
+
+
+
+
+        // ===== Remove Role - || Post || =====================================================================
+         [HttpPost]
+         [Route("RemoveRole")]
+         public async Task<IActionResult> RemoveRole(string roleName, string reAssignRole)
+         {
+            var role = await roleManager.FindByNameAsync(roleName); // Find the role
+            if(role != null)
+            {
+                var roleToDelete = await roleManager.FindByNameAsync(roleName); // Get the role
+                var usersInRole = await userManager.GetUsersInRoleAsync(roleName); // Get List of user in the Role
+
+                     // Reassign Users to another role----------------------------------------------------------
+                    if (usersInRole != null && reAssignRole != null) // If there are users in the role and added role for reassigning the users
+                    {
+                        var roleToReAssign = await roleManager.FindByNameAsync(reAssignRole); // Get the role for reassigning
+                        if(roleToReAssign != null) // If reassigning role exists
+                        {
+                            List<ApplicationUser> rollbackList = new List<ApplicationUser>(); // Rollback list if erors
+                            IdentityResult changeRoleResult;
+
+
+                            for (int i = 0; i < usersInRole.Count; i++) // Loop and reassign users
+                            {
+                               changeRoleResult = await userManager.AddToRoleAsync(usersInRole[i], reAssignRole);
+                               if(changeRoleResult.Succeeded)
+                               {
+                                 rollbackList.Add(usersInRole[i]);
+                               }
+
+
+                               else // If cant reassign users
+                               {
+
+                                   if(rollbackList.Count != 0)  // If list not empty Rollback
+                                   {
+                                      for (int k = 0; k < rollbackList.Count; k++)
+                                      {
+                                        await userManager.AddToRoleAsync(rollbackList[k], roleName);  // Rollback
+                                      }
+                                   }
+                                   return StatusCode(401, "Fatal error: could not reassign one or more of the users - Please try again"); // If list empty return just message
+                               }
+
+                            }
+                        }
+                        else
+                        {
+                           return StatusCode(401, $"The Role: \"{reAssignRole}\" for reassigning the users cant be found");
+                        }
+                    }
+                     
+                
+
+
+                        // Delete the Role-----------------------------------------------------------------------
+                        var result = await roleManager.DeleteAsync(roleToDelete); // Delete Role
+                        if(result.Succeeded) // If Ok
+                        {
+                           return StatusCode(200, $"Role: \"{roleName} \" was Successfully Deleted");  // Role Deleted msg
+                        }
+                           return StatusCode(401, result.Errors); // Delete Errors
+                    
+            }
+            return StatusCode(401,"Role not found"); // Role not found msg
+
+         }
+
+
+
+
+
+
+
+
+
+        // ===== Edit User - In - Role - || Post || =====================================================================
+        [HttpPost]
+        [Route("EditUserInRole")]
+        public async Task<IActionResult> EditUserInRole(string userEmail, string roleName)
+        {
+            var user = await userManager.FindByEmailAsync(userEmail); // Find user
+            if (user != null)
+            {
+                var role = await roleManager.FindByNameAsync(roleName); // Find role
+                if (role != null)
+                {
+                    if (!await userManager.IsInRoleAsync(user, roleName)) // If user is not in that role
+                    {
+                        var result = await userManager.AddToRoleAsync(user, roleName);  // Add the user to the ROLE
+                        if (result.Succeeded)
+                        {
+                            return StatusCode(200, $"The user was Successfully added to the Role: - \"{roleName}\" ");
+                        }
+                    }
+                    return StatusCode(200, "The User is already member of that role"); // Already memeber of that role msg
+                }
+                return StatusCode(401, "Role not found");  // Role not found
+            }
+            return StatusCode(401, "User not found"); // User not found
+        }
+
 
 
 
