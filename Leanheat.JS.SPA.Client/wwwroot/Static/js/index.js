fcd1f22c1e37763efc22a7733cafc1a3f419ffaf
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-02 12:00:28 +0200

    SPA -- > Update

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/index.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/index.js
@@ -2,11 +2,12 @@
 import Dashboard from "./views/Dashboard.js";
 import Posts from "./views/Posts.js";
 import Settings from "./views/Settings.js";
+import Register from "./views/Register.js";
 
 
 // Navigator--------------------------------------------------------------------------------->
 const navigateTo = url => {
-  history.pushState(null, null, url);
+  history.pushState(null, null, url); // Add the url to the history APi of Js
   router();
 };
 
@@ -15,9 +16,10 @@ const navigateTo = url => {
 // Router------------------------------------------------------------------------------------>
 const router = async () => {
  const routes = [
-   {path: "/", view: Dashboard}, // On Path "/" use the dashboard class and inject html
+   {path: "/", view: Dashboard}, // On Path "/" use the dashboard class and inject html in the #app div
    {path: "/posts", view: Posts },
-   {path: "/settings", view: Settings }
+   {path: "/settings", view: Settings },
+   {path: "/Register", view: Register }
  ];
 
  
@@ -25,10 +27,11 @@ const router = async () => {
  
 
  // Test each route for potential match ----------------------------------------------------->
+ // Get the current Url and check if its defined in routes method "Check if its one of our Spa Urls" ----------------------------------------------------->
  const potentialMatches = routes.map(route => {
   return {
       route: route,
-      isMatch: location.pathname === route.path
+      isMatch: location.pathname === route.path  // true if match else false
   };
  });
 
@@ -36,10 +39,10 @@ const router = async () => {
 
 
 // Check if there is Match------------------------------------------------------------------->
- let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);  // Get isMatch from potentialMatches
 
 
- // if no match return to StartPage
+ // If no match return to StartPage
  if(!match)
  {
      match = {
@@ -48,9 +51,9 @@ const router = async () => {
      };
  }
 
- const view = new match.route.view();
+ const view = new match.route.view(); // If match  use the routes array of the router and get the view function for the route
 
- document.querySelector("#app").innerHTML = await view.getHtml();
+ document.querySelector("#app").innerHTML = await view.getHtml();  // Get the #app div and use the view function to inject Html in it from the view class ex."Dashboard, Posts, Settings etc."
  
 };
 
@@ -59,18 +62,18 @@ const router = async () => {
 
 
 // On-Navigating-Back&Forth-Load the Content--Together with the url------------------------------------------------------------------------------------>
-window.addEventListener("popstate", router);
+window.addEventListener("popstate", router); // On popstate "If back button is pressed" use the router array to load back the previeous SPA View
 
 
 // Listen to document fully Loaded
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", e => { //Listen for the body
+  document.body.addEventListener("click", e => { //Listen for click in the body
     if(e.target.matches("[data-link]")){  // If body item was clicked and its data-link decorated
       e.preventDefault();  // Prevent deafult behavior dont follow the link
-      navigateTo(e.target.href);  // Navigate method
+      navigateTo(e.target.href);  // Navigate method   
     }
   });
 
 
-router();
+router(); // Load the content if the url is defined in our "Spa Urls"
 });
---
+++ Leanheat.Spa.Server.Domain/ViewModels/AppUserViewModels/AppUserLogInViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leanheat.Spa.Server.Domain.ViewModels.AppUserViewModels
+{
+    public class AppUserLogInViewModel
+    {
+        // ==== [Email] ==============================================================================================================================
+        [Required(ErrorMessage = "Email is requered")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email is not valid")]  // Custom Email Validation
+        public string Email { get; set; }
+
+      
+
+        // ==== [Password] ===========================================================================================================================
+        [Required(ErrorMessage = "Password is requered")]
+        [RegularExpression(@"^\S{6,40}$", ErrorMessage = "Password should be min 6 characters")]  // Allow all chars exept spaces,tabs etc.  min 6 and max 40 chars
+        public string Password { get; set; }
+
+
+
+        // ==== [Repeat - Password] ==================================================================================================================
+        [Required(ErrorMessage = "Repeat Password is requered")]  
+        [CompareProperty("Password", ErrorMessage = "Password does not match")]
+        public string RepeadPassword { get; set; }
+
+
+        
+
+        // ==== [RememberMe] =========================================================================================================================
+        public bool RememberMe { get; set; }
+    }
+}
