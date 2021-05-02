f382c95a10ed9afa1e085f03706f6f5a28dafc1b
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-02 19:23:37 +0200

    Problem with components folder

---
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/api/api.js
@@ -0,0 +1,29 @@
+﻿
+ 
+
+
+// POST
+async function Post(postData)
+{
+    fetch(postData).then(function (response) {
+        // IF OK
+        if (response.ok) 
+        {
+            return response.json();
+        }
+        else // If Bad STATUS
+        {
+            return Promise.reject(response);
+        }
+
+    }).then(function (responceData) // Responce DATA
+    {
+        // Responce data--->
+
+    }).catch(function (err) // CATCH
+    {
+
+        // Handle error here
+        console.warn('Post Exception:', err);
+    });
+}
