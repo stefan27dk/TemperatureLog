051a3381f97a37875a2d2fb023358b219cea74a9
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-03 20:55:17 +0200

    Succes Message Works not refined

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/components/Messages.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/components/Messages.js
@@ -1,9 +1,12 @@
 ﻿
 
  // Succsess Message
-function SuccessMsg() {
+function SuccessMsg(data) {
+    // Show div with Svg - Msg in body
+    var body = document.getElementsByTagName('body')[0];
+    body.innerHTML += `<div id="successMSG" style="display: block; left: 0; right: 0; margin: auto; top: 100px; padding: 10px; border-radius: 4px; text-align: center; justify-content: center; color: blue; position: fixed; background-color: rgb(190, 240, 137); width: auto; height: auto; max-width: 200px; max-height: 70px; opacity: 0.97; z-index: 100; filter: drop-shadow(0px 0px 10px rgb(129, 255, 112));"> <p>${data}</p> </div >`;
 
-setTimeout(function () {
-    <div class="sucsess-Message"><p> SUCCESS</p></div>
-}, 3000);
+
+    // Remove div after 3 sec
+    setTimeout(function () { document.getElementById("successMSG").remove(); }, 3000);
 }
