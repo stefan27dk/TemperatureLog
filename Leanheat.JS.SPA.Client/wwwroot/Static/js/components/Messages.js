2239915ba6008c8ffc9c78b726f6568970d00eda
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-04 17:15:33 +0200

    Img

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/components/Messages.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/components/Messages.js
@@ -1,12 +1,52 @@
 ﻿
 
- // Succsess Message
+ // =============== || Succsess Message || =================================================================
 function SuccessMsg(data) {
-    // Show div with Svg - Msg in body
+
+    var leftBar = document.getElementById('left-bar');
+    var leftBarWidth = leftBar.offsetWidth +25;
+    
+   
+    // Show div with Msg - Msg in body
     var body = document.getElementsByTagName('body')[0];
-    body.innerHTML += `<div id="successMSG" style="display: block; left: 0; right: 0; margin: auto; top: 100px; padding: 10px; border-radius: 4px; text-align: center; justify-content: center; color: blue; position: fixed; background-color: rgb(190, 240, 137); width: auto; height: auto; max-width: 200px; max-height: 70px; opacity: 0.97; z-index: 100; filter: drop-shadow(0px 0px 10px rgb(129, 255, 112));"> <p>${data}</p> </div >`;
+    body.innerHTML += `<div id="successMSG" class="base-Message sucsess-Message" style="right:calc(50% - ${leftBarWidth}px);"> <p>${data}</p> </div>`;
 
 
     // Remove div after 3 sec
     setTimeout(function () { document.getElementById("successMSG").remove(); }, 3000);
 }
+
+
+
+
+
+
+
+ // =============== || Loading Message || ==================================================================
+function LoadingMsg() {
+
+    var leftBar = document.getElementById('left-bar'); // Get the leftbar
+    var leftBarWidth = leftBar.offsetWidth + 25; // Add 25px to the leftbar width
+
+
+    // Show div with Loading - Msg in body
+    var body = document.getElementsByTagName('body')[0];
+    body.innerHTML += `<div id="loadingMsg" class="base-Message" style="right:calc(50% - ${leftBarWidth}px);><img src="img/Messages/loading-100.gif" /></div>`;
+
+    // Msg is remove if StatusCode = ok or else
+}
+
+
+
+ // ----------------- || Remove Loading MSG || ---------------------------------------------------------------
+function RemoveLoadingMsg() {
+    document.getElementById('loadingMsg').remove();
+}
+
+
+
+
+
+
+
+ // =============== || Error Message || ====================================================================
