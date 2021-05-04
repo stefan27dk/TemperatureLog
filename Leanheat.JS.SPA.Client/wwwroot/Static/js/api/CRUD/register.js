b627d28154d64508c5b4be49c4b356d2ed19c347
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-04 17:15:33 +0200

    Img

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/api/CRUD/register.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/api/CRUD/register.js
@@ -7,30 +7,31 @@
        
         registerForm.addEventListener('submit', function (e)
         {
-            e.preventDefault(); // Prevent page reload on Submit
-        
+            e.preventDefault(); // Prevent page reload on Submit  
+            LoadingMsg(); // Show Loading Message
         
             const formData = new FormData(this); // "this" = this Form
-            const searchParams = new URLSearchParams(formData); // Get the form data params 
-
+            const searchParams = new URLSearchParams(formData); // Get the form data params  
             let formQueryString = searchParams.toString(); // Get the form data params as string
 
 
+            // POST
             fetch(identityApiUri+'/Account/Register?'+formQueryString, {
                 method: 'post'
-            }).then(function (response) {
+            }).then(function (response) {    
 
-              
                 // IF OK                       
                 if (response.status == 201) { // Status 201 = "Created"
+                    RemoveLoadingMsg();
                     SuccessMsg("Success");
                     this.registerForm.reset(); // Reset the Form
                     //return response.json();
                 }
                 else // If Bad STATUS
                 {
+                    RemoveLoadingMsg(); // Remove Loading Message
                     alert("BAD!!!");
-                    console.log("BAD!!!!!");
+                    console.log("Register Error - Could not register");
                     return Promise.reject(response);
                 }
         
