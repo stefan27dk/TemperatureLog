93906f370757d8da5043b1aa288b860757a79883
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-19 14:24:18 +0200

    SPA _ IDENTITY API  -----------------------> OK!!

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/api/api.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/api/api.js
@@ -2,28 +2,28 @@
  
 
 
-// POST
-async function Post(postData)
-{
-    fetch(postData).then(function (response) {
-        // IF OK
-        if (response.ok) 
-        {
-            return response.json();
-        }
-        else // If Bad STATUS
-        {
-            return Promise.reject(response);
-        }
+//// POST
+//async function Post(postData)
+//{
+//    fetch(postData).then(function (response) {
+//        // IF OK
+//        if (response.ok) 
+//        {
+//            return response.json();
+//        }
+//        else // If Bad STATUS
+//        {
+//            return Promise.reject(response);
+//        }
 
-    }).then(function (responceData) // Responce DATA
-    {
-        // Responce data--->
+//    }).then(function (responceData) // Responce DATA
+//    {
+//        // Responce data--->
 
-    }).catch(function (err) // CATCH
-    {
+//    }).catch(function (err) // CATCH
+//    {
 
-        // Handle error here
-        alert(err);
-    });
-}
+//        // Handle error here
+//        alert(err);
+//    });
+//}
