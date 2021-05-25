fbd74611f5a5f752c2ac7c7fb7bd911cfff9914c
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-25 12:55:33 +0200

    Refactoring GET ---> Works--->SPA

--- Leanheat.JS.SPA.Client/wwwroot/Static/js/api/api_adresses.js
+++ Leanheat.JS.SPA.Client/wwwroot/Static/js/api/api_adresses.js
@@ -9,3 +9,9 @@ var identityApiUri = "https://localhost:44347";
 
 // SPA-ServerAPI - Address
 var spaServerApi = "https://localhost:44310";
+
+
+
+
+// Temperature API
+var tempAPI = "https://localhost:44371";
--- Leanheat.JS.SPA.Client/wwwroot/static/js/api/crud/get.js
+++ Leanheat.JS.SPA.Client/wwwroot/static/js/api/crud/get.js
@@ -1,9 +1,11 @@
 ﻿
 
-// ============== || Get User Email || =======================================================================================
-export async function GetUserEmail() {
 
-    return await fetch(identityApiUri + '/Account/GetUserEmail',
+
+// ============== || Default - Get || =======================================================================================
+export async function Get(address, path)
+{
+    return await fetch(address + path,
         {
             method: 'GET',
             mode: 'cors',
@@ -44,6 +46,59 @@ export async function GetUserEmail() {
 
 
 
+
+
+
+
+
+
+
+// ============== || Get User Email || =======================================================================================
+export async function GetUserEmail()
+{
+    return Get(identityApiUri, '/Account/GetUserEmail');
+
+    //return await fetch(identityApiUri + '/Account/GetUserEmail',
+    //    {
+    //        method: 'GET',
+    //        mode: 'cors',
+    //        credentials: 'include' // Allows to return the Cookies with the responce // The Cors should be also set up at the server
+
+    //    }).then(function (response) {
+    //        if (response.status == 200) // IF OK  
+    //        {
+    //            return response;
+    //        }
+    //        else // If Bad STATUS
+    //        {
+    //            return Promise.reject(response);  // Triggers Catch method
+    //        }
+
+    //    }).catch(function (err) // If Exception
+    //    {
+    //        // Show Error
+    //        try // Because of err.text()
+    //        {
+    //            err.text().then(errorMessage => {
+    //                var error = errorMessage.substring(1, errorMessage.length - 1); // Remove the [..] form the Msg
+    //                console.error(error); // Get the error and display
+    //            });
+    //        }
+    //        catch (e) {
+    //            console.warn("Get User Exception -  Probably No connection to hte server");
+    //            console.error(e); // Get the error and display
+    //        }
+    //        console.warn('Post Exception:', err);
+    //    });
+}
+
+
+
+
+
+
+
+
  
 
 
@@ -51,40 +106,44 @@ export async function GetUserEmail() {
 
 
 // ============== || Get User DATA || =======================================================================================
-export async function GetUserData() {
+export async function GetUserData()
+{
 
-    return await fetch(identityApiUri + '/Account/GetUserData',
-        {
-            method: 'GET',
-            mode: 'cors',
-            credentials: 'include' // Allows to return the Cookies with the responce // The Cors should be also set up at the server
+    return Get(identityApiUri, '/Account/GetUserData');
 
-        }).then(function (response) {
-            if (response.status == 200) // IF OK  
-            {
-                return response;
-            }
-            else // If Bad STATUS
-            {
-                return Promise.reject(response);  // Triggers Catch method
-            }
 
-        }).catch(function (err) // If Exception
-        {
-            // Show Error
-            try // Because of err.text()
-            {
-                err.text().then(errorMessage => {
-                    var error = errorMessage.substring(1, errorMessage.length - 1); // Remove the [..] form the Msg
-                    console.error(error); // Get the error and display
-                });
-            }
-            catch (e) {
-                console.warn("Get User Exception -  No connection to hte server");
-                console.error(e); // Get the error and display
-            }
-            console.warn('Post Exception:', err);
-        });
+    //return await fetch(identityApiUri + '/Account/GetUserData',
+    //    {
+    //        method: 'GET',
+    //        mode: 'cors',
+    //        credentials: 'include' // Allows to return the Cookies with the responce // The Cors should be also set up at the server
+
+    //    }).then(function (response) {
+    //        if (response.status == 200) // IF OK  
+    //        {
+    //            return response;
+    //        }
+    //        else // If Bad STATUS
+    //        {
+    //            return Promise.reject(response);  // Triggers Catch method
+    //        }
+
+    //    }).catch(function (err) // If Exception
+    //    {
+    //        // Show Error
+    //        try // Because of err.text()
+    //        {
+    //            err.text().then(errorMessage => {
+    //                var error = errorMessage.substring(1, errorMessage.length - 1); // Remove the [..] form the Msg
+    //                console.error(error); // Get the error and display
+    //            });
+    //        }
+    //        catch (e) {
+    //            console.warn("Get User Exception -  No connection to hte server");
+    //            console.error(e); // Get the error and display
+    //        }
+    //        console.warn('Post Exception:', err);
+    //    });
 }
 
 
@@ -107,7 +166,7 @@ export async function PopulateUpdateForm(formID) {
     var currentForm = document.getElementById(formID);  // Get the form   
     var resPrommise = await GetUserData();  // Get responce
 
-
+       
     // populate the Form
     await resPrommise.json().then(content => // Check the response content
     {
