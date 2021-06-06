d3626edea5b7416c776c6395c10b6e9010f15759
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-06-06 12:34:11 +0200

    Added Routing to Images in the leftBar

---
+++ Leanheat.JS.SPA.Client/wwwroot/static/js/resources/event_listeners.js
@@ -0,0 +1,23 @@
+﻿import {routeTo} from '/static/js/resources/default_scripts.js';
+
+
+
+//====== | LeftBar - Event Listeners | ============================================================
+
+// Dashboard - Img
+document.getElementById('dashboardImg').onclick = function () { routeTo('/') }; // Add Link to the img
+
+// Search - Img
+document.getElementById('searchImg').onclick = function () { routeTo('/Search') }; // Add Link to the img
+
+
+
+// MongoGraph - Img
+document.getElementById('mongographImg').onclick = function () { routeTo('/MongoGraph') }; // Add Link to the img
+
+
+
+
+// Posts- Img
+document.getElementById('postsImg').onclick = function () { routeTo('/Posts') }; // Add Link to the img
+
